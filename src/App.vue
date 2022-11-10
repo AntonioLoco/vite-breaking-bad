548ce6a4ec63c23dc802f8cbb9b7c86a93611618
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-10 14:32:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import axios from "axios";
+
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Ciao</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
